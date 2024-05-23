40be073290a5aa96488991058754e70732bfa199
Mohamed Sboui <akramsboui453@gmail.com>
2024-05-23 02:00:26 +0100

    sdf

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,8 @@
 -- list all comedy shows .
-SELECT t.`title`
-FROM `tv_shows` AS t
-  INNER JOIN `tv_show_genres` AS s ON t.`id` = s.`show_id`
-  INNER JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
-WHERE g.`name` = "Comedy"
-ORDER BY t.`title`;
+SELECT title
+FROM tv_shows
+  LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+  LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
